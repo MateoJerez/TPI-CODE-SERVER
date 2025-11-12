793906a044532868bf2171fb1ccdeb059479fca0
MateoJerez <mateojerez585@gmail.com>
2025-11-12 20:23:35 -0300

    Delete boletin-digital directory

--- boletin-digital/middleware/role.middleware.js
+++
@@ -1,9 +0,0 @@
-function allowRoles(...rolesAllowed) {
-  return (req, res, next) => {
-    const rol = Number(req.usuario && req.usuario.id_rol);
-    if (!rolesAllowed.includes(rol)) return res.status(403).json({ ok: false, msg: 'Acceso restringido' });
-    next();
-  };
-}
-
-module.exports = { allowRoles };
--- boletin-digital/routes/materia.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { listarMaterias } = require('../controllers/materia.controller');
-const { verifyToken } = require('../middleware/auth.middleware');
-
-router.get('/', verifyToken, listarMaterias);
-
-module.exports = router;
