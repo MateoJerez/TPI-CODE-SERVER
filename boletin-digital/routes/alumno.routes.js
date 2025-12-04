253e6090eb147a24d7547f482972e683735de97c
MateoJerez <mateojerez585@gmail.com>
2025-12-04 13:08:42 -0300

    Delete boletin-digital directory

--- boletin-digital/routes/alumno.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { listarAlumnos } = require('../controllers/alumno.controller');
-const { verifyToken } = require('../middleware/auth.middleware');
-
-router.get('/', verifyToken, listarAlumnos);
-
-module.exports = router;
